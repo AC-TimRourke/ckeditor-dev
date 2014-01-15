f89d041c5e9d61454f4cc8345195f5194bb1a84c
Peter Evans <pevans@activecampaign.com>
2014-01-15 17:45:29 -0600

    Adding our close/personalization plugins

---
+++ plugins/acClose/plugin.js
@@ -0,0 +1,41 @@
+﻿/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+/**
+ * @fileOverview The "selectall" plugin provides an editor command that
+ *               allows selecting the entire content of editable area.
+ *               This plugin also enables a toolbar button for the feature.
+ */
+
+(function() {
+	CKEDITOR.plugins.add( 'acClose', {
+		icons: 'acClose', // %REMOVE_LINE_CORE%
+		init: function( editor ) {
+			editor.addCommand( 'acClose', { modes:{wysiwyg:1,source:1 },
+				exec: function( editor ) {
+					//console.log($J(editor.element.$).data("partid"));
+					var objid = editor.element.$;
+					var obj = $J(editor.element.$);
+					obj
+						.blur()
+						.removeClass('cke_currentlyediting')
+						.css({ boxShadow: 'none' })
+					;
+					designer.mustsave("part", obj.data("partid"));
+					designer.save();
+				},
+				canUndo: false
+			});
+
+			editor.ui.addButton && editor.ui.addButton( 'acClose', {
+				label: 'Close edit mode (Saves Automatically)',
+				command: 'acClose',
+				toolbar: 'links,10'
+			});
+
+			//ac.ck.buttonForceLabel('acClose');
+		}
+	});
+})();
---
+++ plugins/acPersonalize/plugin.js
@@ -0,0 +1,32 @@
+﻿/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+/**
+ * @fileOverview The "selectall" plugin provides an editor command that
+ *               allows selecting the entire content of editable area.
+ *               This plugin also enables a toolbar button for the feature.
+ */
+
+(function() {
+	CKEDITOR.plugins.add( 'acPersonalize', {
+		//icons: 'acPersonalize', // %REMOVE_LINE_CORE%
+		init: function( editor ) {
+			editor.addCommand( 'acPersonalize', { modes:{wysiwyg:1,source:1 },
+				exec: function( editor ) {
+					designer.perstag.openmodal(editor.element.$.id);
+				},
+				canUndo: true
+			});
+
+			editor.ui.addButton && editor.ui.addButton( 'acPersonalize', {
+				label: 'Personalize',
+				command: 'acPersonalize',
+				toolbar: 'links,10'
+			});
+
+			ac.ck.buttonForceLabel('acPersonalize');
+		}
+	});
+})();
